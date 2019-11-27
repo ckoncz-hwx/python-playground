729dea0191e0548c14dfbf52a3e61459b01510ab
Csaba Koncz <ckoncz@cloudera.com>
2019-11-27 20:30:48 +0100

    Elevator state machine example

---
+++ sm/elevator.py
@@ -0,0 +1,40 @@
+from sismic.io import import_from_yaml
+from sismic.interpreter import Interpreter
+
+def print_info(step):
+    print("\nInfo:")
+    for attribute in ['event', 'transitions', 'entered_states', 'exited_states', 'sent_events']:
+        print('  {}: {}'.format(attribute, getattr(step, attribute)))
+
+def print_events(interpreter):
+    print("\nEvents:")
+    for _, event in interpreter._external_queue:
+       print("  event = %s"%event.name)
+
+
+# Load statechart from yaml file
+elevator = import_from_yaml(filepath='elevator.yaml')
+
+# Create an interpreter for this statechart
+interpreter = Interpreter(elevator)
+
+print('Before:', interpreter.configuration)
+
+step = interpreter.execute_once()
+
+print('After:', interpreter.configuration)
+
+print_info(step)
+
+interpreter.queue('click', 'clack', 'clock')
+print('filled queue')
+
+print_events(interpreter)
+
+while True:
+    step = interpreter.execute_once()
+    if step:
+        print_info(step)
+        print_events(interpreter)
+    else:
+        break
