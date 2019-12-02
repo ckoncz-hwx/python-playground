dd26b30891db76cd726279734f8500a1f58820c4
matekoncz <matekoncz1@gmail.com>
2019-12-02 18:23:32 +0000

    file autosave

---
+++ nemest.py
@@ -0,0 +1,18 @@
+from sys import stdin, stdout
+
+nk = stdin.readline().split()
+n = int (nk[0])
+k = int (nk[1])
+tomeg = []
+ar = []
+osszes = []
+minim = []
+
+for i in range (n):
+    mostani = stdin.readline().split
+    tomeg.append(int (mostani[0]))
+    ar.append(int (mostani[1]))
+    osszes.append(int (mostani[2]))
+    minim.append(int (mostani[3]))
+
+
