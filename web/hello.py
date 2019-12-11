c896a2865be812d6d3ccdf9a77a3826e1345ef70
matekoncz <matekoncz1@gmail.com>
2019-12-11 19:15:40 +0000

    chicken arithmetics

--- web/hello.py
+++ web/hello.py
@@ -1,3 +1,19 @@
+import browser
 from browser import document
+import math
 
-document.body.innerText = "Hello"
+lista = [int (i) for i in browser.prompt("Kérem a tyúkinfót").split()]
+# lista = [int (i) for i in document['bemenet'].value.split()]
+n = lista[0]
+t = lista[1]
+s = lista[2]
+
+p1 = (50+math.sqrt(2500-200*s))/2
+if (p1>30):
+    p1=""
+p2 = (50-math.sqrt(2500-200*s))/2
+if (p2>30):
+    p2=""
+
+print(p1,p2)
+document.body.innerHTML += "<br>Tyúkeredmény %s %s"%(p1,p2)
