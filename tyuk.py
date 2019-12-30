b1a019bb7f81d0eefa88f87b4da3f05d0a681b83
matekoncz <matekoncz1@gmail.com>
2019-12-30 12:29:09 +0000

    chickens 2

--- tyuk.py
+++ tyuk.py
@@ -1,10 +1,7 @@
 from sys import stdin, stdout
 import math
 
-lista = [int (i) for i in stdin.readline().split()]
-n = lista[0]
-t = lista[1]
-s = lista[2]
+s = int(stdin.readline())
 
 p1 = (50+math.sqrt(2500-200*s))/2
 if (p1>30):
